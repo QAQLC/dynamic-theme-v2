c5b166e9d1a1c69ce64566245d341073194a3fef
QAQLC <873228767@qq.com>
2023-05-17 16:26:01 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).use(Antd).mount('#app')
